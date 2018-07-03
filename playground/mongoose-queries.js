25ba0c481c5b7d352ceb7ed96b7a34458287f5e3
Geet207 <kitesom19@gmail.com>
2018-07-03 08:06:10 +0530

    get todo:id api

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,30 +3,30 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-// var id = '5b38f105008d907414cca766';
+var id = '5b38f105008d907414cca766';
 
-// if(!ObjectID.isValid(id)){
-// 	console.log('Id not valid');
-// }
+if(!ObjectID.isValid(id)){
+	console.log('Id not valid');
+}
 
-// Todo.find({
-// 	_id : id
-// }).then((todos) => {
-// 	console.log('Todos', todos);
-// });
+Todo.find({
+	_id : id
+}).then((todos) => {
+	console.log('Todos', todos);
+});
 
-// Todo.findOne({
-// 	_id : id
-// }).then((todo) => {
-// 	console.log('Todo', todo);
-// });
+Todo.findOne({
+	_id : id
+}).then((todo) => {
+	console.log('Todo', todo);
+});
 
-// Todo.findById(id).then((todo) => {
-// 	if(!todo){
-// 		return console.log('Id not found');
-// 	}
-// 	console.log('Todo By Id', todo);
-// }).catch((e) => console.log(e));
+Todo.findById(id).then((todo) => {
+	if(!todo){
+		return console.log('Id not found');
+	}
+	console.log('Todo By Id', todo);
+}).catch((e) => console.log(e));
 
 var user_id = '5b2930b0b065e7440fcfad75';
 User.findById(user_id).then((user) => {
